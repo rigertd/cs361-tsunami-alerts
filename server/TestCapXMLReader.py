288ae7ea9ffae8f4f3e2962e1e1b66fbf8dda980
David Rigert <rigertd@onid.orst.edu>
2016-07-31 09:55:04 -0700

    Updated unit tests to reflect implementation updates.

--- server/TestCapXMLReader.py
+++ server/TestCapXMLReader.py
@@ -30,9 +30,9 @@ def test_init_extra_arg():
 def test_reset_happy_path():
     parser = CapXMLReader()
     parser.add_by_lat_long(0.0, 0.0)
-    assert_equal(len(parser.data['areas']), 1)
+    assert_equal(len(parser.data['locations']), 1)
     parser.reset()
-    assert_equal(len(parser.data['areas']), 0)
+    assert_equal(len(parser.data['locations']), 0)
 
 #================================================
 # get_alert_data Unit Tests
@@ -59,6 +59,7 @@ class Test_get_alert_data(object):
         self.parser.reset()
 
     def test_happy_path(self):
+        self.parser.data['id'] = 'test'
         assert_true(self.parser.add_by_lat_long(44.564089096, -123.28061099))
         assert_equal(self.parser.get_alert_data(), self.parser.data)
 
@@ -91,83 +92,83 @@ class Test_add_by_lat_long(object):
 
     def test_floats_happy_path(self):
         assert_true(self.parser.add_by_lat_long(44.564089096, -123.28061099))
-        assert_equal(len(self.parser.data['areas']), 1) # one item
-        assert_almost_equal(self.parser.data['areas'][0]['latitude'], 44.564089096)
-        assert_almost_equal(self.parser.data['areas'][0]['longitude'], -123.28061099)
+        assert_equal(len(self.parser.data['locations']), 1) # one item
+        assert_almost_equal(self.parser.data['locations'][0][0], 44.564089096)
+        assert_almost_equal(self.parser.data['locations'][0][1], -123.28061099)
 
     def test_floats_max_values(self):
         assert_true(self.parser.add_by_lat_long(90.0, 180.0))
-        assert_equal(len(self.parser.data['areas']), 1) # one item
-        assert_almost_equal(self.parser.data['areas'][0]['latitude'], 90.0)
-        assert_almost_equal(self.parser.data['areas'][0]['longitude'], 180.0)
+        assert_equal(len(self.parser.data['locations']), 1) # one item
+        assert_almost_equal(self.parser.data['locations'][0][0], 90.0)
+        assert_almost_equal(self.parser.data['locations'][0][1], 180.0)
 
     def test_floats_min_values(self):
         assert_true(self.parser.add_by_lat_long(-90.0, -180.0))
-        assert_equal(len(self.parser.data['areas']), 1) # one item
-        assert_almost_equal(self.parser.data['areas'][0]['latitude'], -90.0)
-        assert_almost_equal(self.parser.data['areas'][0]['longitude'], -180.0)
+        assert_equal(len(self.parser.data['locations']), 1) # one item
+        assert_almost_equal(self.parser.data['locations'][0][0], -90.0)
+        assert_almost_equal(self.parser.data['locations'][0][1], -180.0)
 
     def test_floats_lat_too_high(self):
         assert_false(self.parser.add_by_lat_long(90.1, 0.0))
-        assert_equal(len(self.parser.data['areas']), 0) # zero items
+        assert_equal(len(self.parser.data['locations']), 0) # zero items
 
     def test_floats_lat_too_low(self):
         assert_false(self.parser.add_by_lat_long(-90.1, 0.0))
-        assert_equal(len(self.parser.data['areas']), 0) # zero items
+        assert_equal(len(self.parser.data['locations']), 0) # zero items
 
     def test_floats_long_too_high(self):
         assert_false(self.parser.add_by_lat_long(0.0, 180.1))
-        assert_equal(len(self.parser.data['areas']), 0) # zero items
+        assert_equal(len(self.parser.data['locations']), 0) # zero items
 
     def test_floats_long_too_low(self):
         assert_false(self.parser.add_by_lat_long(0.0, -180.1))
-        assert_equal(len(self.parser.data['areas']), 0) # zero items
+        assert_equal(len(self.parser.data['locations']), 0) # zero items
 
     def test_strings_happy_path(self):
         assert_true(self.parser.add_by_lat_long('44.564089096', '-123.28061099'))
-        assert_equal(len(self.parser.data['areas']), 1) # one item
-        assert_almost_equal(self.parser.data['areas'][0]['latitude'], 44.564089096)
-        assert_almost_equal(self.parser.data['areas'][0]['longitude'], -123.28061099)
+        assert_equal(len(self.parser.data['locations']), 1) # one item
+        assert_almost_equal(self.parser.data['locations'][0][0], 44.564089096)
+        assert_almost_equal(self.parser.data['locations'][0][1], -123.28061099)
 
     def test_strings_max_values(self):
         assert_true(self.parser.add_by_lat_long('90.0', '180.0'))
-        assert_equal(len(self.parser.data['areas']), 1) # one item
-        assert_almost_equal(self.parser.data['areas'][0]['latitude'], 90.0)
-        assert_almost_equal(self.parser.data['areas'][0]['longitude'], 180.0)
+        assert_equal(len(self.parser.data['locations']), 1) # one item
+        assert_almost_equal(self.parser.data['locations'][0][0], 90.0)
+        assert_almost_equal(self.parser.data['locations'][0][1], 180.0)
 
     def test_strings_min_values(self):
         assert_true(self.parser.add_by_lat_long('-90.0', '-180.0'))
-        assert_equal(len(self.parser.data['areas']), 1) # one item
-        assert_almost_equal(self.parser.data['areas'][0]['latitude'], -90.0)
-        assert_almost_equal(self.parser.data['areas'][0]['longitude'], -180.0)
+        assert_equal(len(self.parser.data['locations']), 1) # one item
+        assert_almost_equal(self.parser.data['locations'][0][0], -90.0)
+        assert_almost_equal(self.parser.data['locations'][0][1], -180.0)
 
     def test_strings_lat_too_high(self):
         assert_false(self.parser.add_by_lat_long('90.1', '0.0'))
-        assert_equal(len(self.parser.data['areas']), 0) # zero items
+        assert_equal(len(self.parser.data['locations']), 0) # zero items
 
     def test_strings_lat_too_low(self):
         assert_false(self.parser.add_by_lat_long('-90.1', '0.0'))
-        assert_equal(len(self.parser.data['areas']), 0) # zero items
+        assert_equal(len(self.parser.data['locations']), 0) # zero items
 
     def test_strings_long_too_high(self):
         assert_false(self.parser.add_by_lat_long('0.0', '180.1'))
-        assert_equal(len(self.parser.data['areas']), 0) # zero items
+        assert_equal(len(self.parser.data['locations']), 0) # zero items
 
     def test_strings_long_too_low(self):
         assert_false(self.parser.add_by_lat_long('0.0', '-180.1'))
-        assert_equal(len(self.parser.data['areas']), 0) # zero items
+        assert_equal(len(self.parser.data['locations']), 0) # zero items
 
     def test_non_numeric_lat(self):
         assert_false(self.parser.add_by_lat_long('asdf', '0.0'))
-        assert_equal(len(self.parser.data['areas']), 0) # zero items
+        assert_equal(len(self.parser.data['locations']), 0) # zero items
 
     def test_non_numeric_long(self):
         assert_false(self.parser.add_by_lat_long('0.0', 'asdf'))
-        assert_equal(len(self.parser.data['areas']), 0) # zero items
+        assert_equal(len(self.parser.data['locations']), 0) # zero items
 
     def test_non_numeric_both(self):
         assert_false(self.parser.add_by_lat_long('asdf', 'asdf'))
-        assert_equal(len(self.parser.data['areas']), 0) # zero items
+        assert_equal(len(self.parser.data['locations']), 0) # zero items
 
 #================================================
 # add_by_address Unit Tests
@@ -195,35 +196,29 @@ class Test_add_by_address(object):
 
     def test_city_state_zip_happy_path(self):
         assert_true(self.parser.add_by_address('Corvallis, OR 97331'))
-        assert_equal(len(self.parser.data['areas']), 1) # one item
-        assert_almost_equal(self.parser.data['areas'][0]['latitude'], 44.564089096)
-        assert_almost_equal(self.parser.data['areas'][0]['longitude'], -123.28061099)
+        assert_equal(len(self.parser.data['locations']), 1) # one item
+        assert_almost_equal(self.parser.data['locations'][0][0], 44.564089096)
+        assert_almost_equal(self.parser.data['locations'][0][1], -123.28061099)
 
     def test_full_address_happy_path(self):
         assert_true(self.parser.add_by_address('500 SW Jefferson Way, Corvallis, OR 97331'))
-        assert_equal(len(self.parser.data['areas']), 1) # one item
-        assert_almost_equal(self.parser.data['areas'][0]['latitude'], 44.5646016)
-        assert_almost_equal(self.parser.data['areas'][0]['longitude'], -123.27254)
+        assert_equal(len(self.parser.data['locations']), 1) # one item
+        assert_almost_equal(self.parser.data['locations'][0][0], 44.5646016)
+        assert_almost_equal(self.parser.data['locations'][0][1], -123.27254)
 
     def test_invalid_address(self):
         assert_false(self.parser.add_by_address('adskfjs;ldkjf;lkasjdf'))
-        assert_equal(len(self.parser.data['areas']), 0) # Should be empty
+        assert_equal(len(self.parser.data['locations']), 0) # Should be empty
 
     def test_incorrect_zip(self):
         assert_false(self.parser.add_by_address('Corvallis, OR 98108'))
-        assert_equal(len(self.parser.data['areas']), 0) # Should be empty
+        assert_equal(len(self.parser.data['locations']), 0) # Should be empty
 
     def test_lat_long_string(self):
         assert_true(self.parser.add_by_address('44.564089096,-123.28061099'))
-        assert_equal(len(self.parser.data['areas']), 1) # one item
-        assert_almost_equal(self.parser.data['areas'][0]['latitude'], 44.564089096)
-        assert_almost_equal(self.parser.data['areas'][0]['longitude'], -123.28061099)
-
-    def test_lat_long_string_with_radius(self):
-        assert_true(self.parser.add_by_address('44.564089096,-123.28061099 0.0'))
-        assert_equal(len(self.parser.data['areas']), 1) # one item
-        assert_almost_equal(self.parser.data['areas'][0]['latitude'], 44.564089096)
-        assert_almost_equal(self.parser.data['areas'][0]['longitude'], -123.28061099)
+        assert_equal(len(self.parser.data['locations']), 1) # one item
+        assert_almost_equal(self.parser.data['locations'][0][0], 44.564089096, places=3)
+        assert_almost_equal(self.parser.data['locations'][0][1], -123.28061099, places=3)
 
 #================================================
 # parse Unit Tests
@@ -247,17 +242,20 @@ class Test_parse_real_alert(object):
         """
         self.parser = None
 
-    def test_date(self):
-        assert_equal(self.parser.data['date'], datetime(2012, 10, 28, 3, 7, 27, tzinfo=tz.tzutc()))
+    def test_onsetDate(self):
+        assert_equal(self.parser.data['onsetDate'], datetime(2012, 10, 28, 3, 7, 27, tzinfo=tz.tzutc()))
 
-    def test_status(self):
-        assert_equal(self.parser.data['status'], 'Actual')
+    def test_expireDate(self):
+        assert_equal(self.parser.data['expireDate'], datetime(2012, 10, 28, 5, 7, 27, tzinfo=tz.tzutc()))
 
-    def test_update_flag(self):
+    def test_isUpdate_false(self):
         assert_false(self.parser.data['isUpdate'])
 
-    def test_alert_type(self):
-        assert_equal(self.parser.data['type'], 'Warning')
+    def test_cancel_flag(self):
+        assert_false(self.parser.data['isCancel'])
+
+    def test_id(self):
+        assert_equal(self.parser.data['id'], 'mcl1uw')
 
 class Test_parse_real_update(object):
     @classmethod
@@ -278,18 +276,20 @@ class Test_parse_real_update(object):
         """
         self.parser = None
 
+    def test_onsetDate(self):
+        assert_equal(self.parser.data['onsetDate'], datetime(2012, 10, 28, 3, 34, 51, tzinfo=tz.tzutc()))
 
-    def test_date(self):
-        assert_equal(self.parser.data['date'], datetime(2012, 10, 28, 3, 34, 51, tzinfo=tz.tzutc()))
-
-    def test_status(self):
-        assert_equal(self.parser.data['status'], 'Actual')
+    def test_expireDate(self):
+        assert_equal(self.parser.data['expireDate'], datetime(2012, 10, 28, 5, 34, 51, tzinfo=tz.tzutc()))
 
     def test_update_flag(self):
         assert_true(self.parser.data['isUpdate'])
 
-    def test_alert_type(self):
-        assert_equal(self.parser.data['type'], 'Warning')
+    def test_cancel_flag(self):
+        assert_false(self.parser.data['isCancel'])
+
+    def test_id(self):
+        assert_equal(self.parser.data['id'], 'mcl1uw')
 
 class Test_parse_real_cancel(object):
     @classmethod
@@ -310,42 +310,29 @@ class Test_parse_real_cancel(object):
         """
         self.parser = None
 
-    def test_date(self):
-        assert_equal(self.parser.data['date'], datetime(2012, 10, 28, 11, 44, 34, tzinfo=tz.tzutc()))
+    def test_onsetDate(self):
+        assert_equal(self.parser.data['onsetDate'], datetime(2012, 10, 28, 11, 44, 34, tzinfo=tz.tzutc()))
 
-    def test_status(self):
-        assert_equal(self.parser.data['status'], 'Actual')
+    def test_expireDate(self):
+        assert_equal(self.parser.data['expireDate'], datetime(2012, 10, 28, 14, 44, 34, tzinfo=tz.tzutc()))
 
     def test_update_flag(self):
         assert_true(self.parser.data['isUpdate'])
 
-    def test_alert_type(self):
-        assert_equal(self.parser.data['type'], 'Cancellation')
+    def test_cancel_flag(self):
+        assert_true(self.parser.data['isCancel'])
 
-class Test_parse_real_cancel(object):
-    @classmethod
-    def setup_class(self):
-        """
-        Setup code run before any tests are run
-        """
-        self.parser = CapXMLReader()
-        f = open(r'testdata/exercise.xml', 'r')
-        data = f.read()
-        f.close()
-        self.parser.parse(data)
+    def test_id(self):
+        assert_equal(self.parser.data['id'], 'mcl1uw')
 
-    @classmethod
-    def teardown_class(self):
-        """
-        Teardown code run after all tests are run
-        """
-        self.parser = None
-
-    def test_date(self):
-        assert_equal(self.parser.data['date'], datetime(2012, 10, 28, 3, 7, 27, tzinfo=tz.tzutc()))
-
-    def test_status(self):
-        assert_equal(self.parser.data['status'], 'Exercise')
+def test_parse_exercise_data():
+    parser = CapXMLReader()
+    f = open(r'testdata/exercise.xml', 'r')
+    data = f.read()
+    f.close()
+    assert_false(parser.parse(data))
+    assert_equal(parser.get_alert_data(), None)
+    parser = None
 
 def test_parse_invalid_data():
     parser = CapXMLReader()
