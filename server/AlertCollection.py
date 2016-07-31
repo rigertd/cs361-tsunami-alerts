fc151a435d5096d38ce571af8543f6336e419247
David Rigert <rigertd@onid.orst.edu>
2016-07-31 09:54:01 -0700

    Converted tabs to spaces.

--- server/AlertCollection.py
+++ server/AlertCollection.py
@@ -9,44 +9,44 @@ class to track collections of alerts.
 import Alert
 
 class AlertCollection:
-	"""
-	Holds collection of alerts
+    """
+    Holds collection of alerts
 
-	Class variables:
-	alerts -- list of Alert objects
-	"""
-	def __init__(self):
-		self.alerts = {}
+    Class variables:
+    alerts -- list of Alert objects
+    """
+    def __init__(self):
+        self.alerts = {}
 
-	def add_alert_by_object(self, alert):
-		"""
-		Adds passed-in alert object to AlertCollection.
-		"""
-		self.alerts[alert.id] = alert
+    def add_alert_by_object(self, alert):
+        """
+        Adds passed-in alert object to AlertCollection.
+        """
+        self.alerts[alert.id] = alert
 
-	def add_alert_by_config(self, config):
-		"""
-		Creates an Alert from passed-in config and adds
-		it to AlertCollection.
-		"""
-		alert = Alert(config)
-		self.add_alert_object(alert)
+    def add_alert_by_config(self, config):
+        """
+        Creates an Alert from passed-in config and adds
+        it to AlertCollection.
+        """
+        alert = Alert(config)
+        self.add_alert_object(alert)
 
-	def remove_alert_by_id(self, alertID):
-		"""
-		Removes Alert from AlertCollection that matches
-		the passed-in id.
-		"""
+    def remove_alert_by_id(self, alertID):
+        """
+        Removes Alert from AlertCollection that matches
+        the passed-in id.
+        """
         del self.alerts[alertID]
 
     def get_alerts_in_range(self, location, mileRange):
-    	"""
-    	Returns dictionary of Alerts keyed by alert id 
-    	that are within the passed-in mileRange of the
-    	passed-in location.
-    	"""
-    	alertsInRange = {}
-    	for alertID, alert in self.alerts:
-    		if alert.is_in_range(location, mileRange):
-    			alertsInRange[alert.get_id()] = alert
-    	return alertsInRange
+        """
+        Returns dictionary of Alerts keyed by alert id 
+        that are within the passed-in mileRange of the
+        passed-in location.
+        """
+        alertsInRange = {}
+        for alertID, alert in self.alerts:
+            if alert.is_in_range(location, mileRange):
+                alertsInRange[alert.get_id()] = alert
+        return alertsInRange
--- server/TestAlert.py
+++ server/TestAlert.py
@@ -20,46 +20,46 @@ from dateutil.parser import parse
 # Initialization Unit Tests
 #================================================
 def test_init_happy_path():
-	alert = Alert("1997-07-16T19:20+01:00")
-	assert_not_equal(alert, None)
-	assert_equal(alert.onsetDate, parse("1997-07-16T19:20+01:00"))
+    alert = Alert("1997-07-16T19:20+01:00")
+    assert_not_equal(alert, None)
+    assert_equal(alert.onsetDate, parse("1997-07-16T19:20+01:00"))
 
 @raises(TypeError)
 def test_init_no_args():
-	alert = Alert()
-	assert_equal(alert, None)
+    alert = Alert()
+    assert_equal(alert, None)
 
 def test_init_expireDate_happy_path():
-	alert = Alert("1997-07-16T19:20+01:00", "1997-07-16T21:20+01:00")
-	assert_not_equal(alert, None)
-	assert_equal(alert.expireDate, parse("1997-07-16T21:20+01:00"))
+    alert = Alert("1997-07-16T19:20+01:00", "1997-07-16T21:20+01:00")
+    assert_not_equal(alert, None)
+    assert_equal(alert.expireDate, parse("1997-07-16T21:20+01:00"))
 
 @raises(TypeError)
 def test_init_extra_arg():
-	alert = Alert("1997-07-16T19:20+01:00", "1997-07-16T21:20+01:00", "bar")
-	assert_equal(alert, None)
+    alert = Alert("1997-07-16T19:20+01:00", "1997-07-16T21:20+01:00", "bar")
+    assert_equal(alert, None)
 
 def test_add_location_happy_path():
-	alert = Alert("1997-07-16T19:20+01:00")
-	assert_equal(len(alert.locations), 0)
-	alert.add_location(00.00, 00.00)
-	assert_equal(len(alert.locations), 1)
+    alert = Alert("1997-07-16T19:20+01:00")
+    assert_equal(len(alert.locations), 0)
+    alert.add_location(00.00, 00.00)
+    assert_equal(len(alert.locations), 1)
 
 def test_remove_location_happy_path():
-	alert = Alert("1997-07-16T19:20+01:00")
-	alert.add_location(00.00, 00.00)
-	assert_equal(len(alert.locations), 1)
-	alert.remove_location(00.00, 00.00)
-	assert_equal(len(alert.locations), 0)
+    alert = Alert("1997-07-16T19:20+01:00")
+    alert.add_location(00.00, 00.00)
+    assert_equal(len(alert.locations), 1)
+    alert.remove_location(00.00, 00.00)
+    assert_equal(len(alert.locations), 0)
 
 def test_update_onsetDate_happy_path():
-	alert = Alert("1997-07-16T19:20+01:00")
-	assert_equal(alert.onsetDate, parse("1997-07-16T19:20+01:00"))
-	alert.update_onsetDate("1998-07-16T19:20+01:00")
-	assert_equal(alert.onsetDate, parse("1998-07-16T19:20+01:00"))
+    alert = Alert("1997-07-16T19:20+01:00")
+    assert_equal(alert.onsetDate, parse("1997-07-16T19:20+01:00"))
+    alert.update_onsetDate("1998-07-16T19:20+01:00")
+    assert_equal(alert.onsetDate, parse("1998-07-16T19:20+01:00"))
 
 def test_update_expireDate_happy_path():
-	alert = Alert("1997-07-16T19:20+01:00", "2000-07-16T19:20+01:00")
-	assert_equal(alert.expireDate, parse("2000-07-16T19:20+01:00"))
-	alert.update_expireDate("1997-08-16T19:20+01:00")
-	assert_equal(alert.expireDate, parse("1997-08-16T19:20+01:00"))
+    alert = Alert("1997-07-16T19:20+01:00", "2000-07-16T19:20+01:00")
+    assert_equal(alert.expireDate, parse("2000-07-16T19:20+01:00"))
+    alert.update_expireDate("1997-08-16T19:20+01:00")
+    assert_equal(alert.expireDate, parse("1997-08-16T19:20+01:00"))
