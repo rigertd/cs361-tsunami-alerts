51d4f684847b442c793d73096de28c3f420c3b0b
Sammy Pettinichi <nutarex@gmail.com>
2016-08-02 19:24:29 -0400

    Tabs to Spaces

--- server/TsuShield.py
+++ server/TsuShield.py
@@ -35,11 +35,11 @@ class AlertServerApplication(webapp2.RequestHandler):
             self.response.write("Invalid coordinates. Cannot complete request.")
             return
 			
-		if latitude > 90 or latitude < -90:
-			self.response.write("Invalid coordinates. Cannot complete request.")
+        if latitude > 90 or latitude < -90:
+            self.response.write("Invalid coordinates. Cannot complete request.")
             return
-		else if longitude > 180 or longitude < -180:
-			self.response.write("Invalid coordinates. Cannot complete request.")
+        else if longitude > 180 or longitude < -180:
+            self.response.write("Invalid coordinates. Cannot complete request.")
             return
 
         alertsInRange = alerts.get_alerts_in_range((latitude, longitude), 10.0)
--- server/TsuShieldTest.py
+++ server/TsuShieldTest.py
@@ -18,7 +18,7 @@ from datetime import datetime
 from dateutil.parser import parse
 
 def test_download_alert_data():
-	assert_not_equal(alert, None)
+    assert_not_equal(alert, None)
 
 app = webapp2.WSGIApplication([('/', AlertServerApplication),], debug=True)
 host = '158.69.197.74'
@@ -26,36 +26,36 @@ port = '8080'
 httpserver.serve(app, host, port)
 
 def test_download_alert_data_happy_path():
-	assert_not_equal(alerts, None)
+    assert_not_equal(alerts, None)
 	
 	
 def test_alert_server_application_happy_path():
-	url = 'http://vps54981.vps.ovh.ca:8080/?latitude=54.266701&longitude=-133.066696'
-	
-	response = urllib2.urlopen(url)
+    url = 'http://vps54981.vps.ovh.ca:8080/?latitude=54.266701&longitude=-133.066696'
+    
+    response = urllib2.urlopen(url)
     alert = response.read()
-	assert_not_equal(alert, None)
+    assert_not_equal(alert, None)
 	
 def test_alert_server_application_invalid_type():
-	url = 'http://vps54981.vps.ovh.ca:8080/?latitude=foo&longitude=bar'
-	
-	response = urllib2.urlopen(url)
+    url = 'http://vps54981.vps.ovh.ca:8080/?latitude=foo&longitude=bar'
+    
+    response = urllib2.urlopen(url)
     alert = response.read()
-	assert_not_equal(alert, None)
-	assert_equal(alert,"Invalid coordinates. Cannot complete request.")
+    assert_not_equal(alert, None)
+    assert_equal(alert,"Invalid coordinates. Cannot complete request.")
 	
 def test_alert_server_application_invalid_latitude():
-	url = 'http://vps54981.vps.ovh.ca:8080/?latitude=91&longitude=1'
-	
-	response = urllib2.urlopen(url)
+    url = 'http://vps54981.vps.ovh.ca:8080/?latitude=91&longitude=1'
+    
+    response = urllib2.urlopen(url)
     alert = response.read()
-	assert_not_equal(alert, None)
-	assert_equal(alert,"Invalid coordinates. Cannot complete request.")
+    assert_not_equal(alert, None)
+    assert_equal(alert,"Invalid coordinates. Cannot complete request.")
 	
 def test_alert_server_application_invalid_longitude():
-	url = 'http://vps54981.vps.ovh.ca:8080/?latitude=1&longitude=-181'
-	
-	response = urllib2.urlopen(url)
+    url = 'http://vps54981.vps.ovh.ca:8080/?latitude=1&longitude=-181'
+    
+    response = urllib2.urlopen(url)
     alert = response.read()
-	assert_not_equal(alert, None)
-	assert_equal(alert,"Invalid coordinates. Cannot complete request.")
+    assert_not_equal(alert, None)
+    assert_equal(alert,"Invalid coordinates. Cannot complete request.")
